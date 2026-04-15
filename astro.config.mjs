0d976b961cbe0583d1d3aa3f337ed8f3587f7680
Kevin Johnson <kevin@kj800x.com>
2026-04-15 01:14:40 -0400

    Build files

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	site: 'https://blog.coolkev.com',
 	integrations: [mdx(), sitemap()],
+	build: {
+		format: 'file',
+	},
 });
